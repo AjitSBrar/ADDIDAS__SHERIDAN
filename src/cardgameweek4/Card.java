f2509a6373010e122d7b56676587b3890bf4ff7b
AjitSBrar <50676263+AjitSBrar@users.noreply.github.com>
2019-06-12 15:11:28 -0400

    Update Card.java

--- src/cardgameweek4/Card.java
+++ src/cardgameweek4/Card.java
@@ -21,15 +21,15 @@ public class Card {
     }//end getCard
     
     //setter method
-    public void setCard(int gVal){
+    public void setValue(int gVal){
         value = gVal;
-    }//end setCard
+    }//end setValue
     
     @Override
     public String toString(){
     //combine rank and suit together into a single string(ex: Ace of Diamonds)
 
-        //suing StringBuilder for modifiability later on
+        //suing StringBuilder 
         StringBuilder display = new StringBuilder();
         
         //personal choice to use switch
@@ -48,11 +48,11 @@ public class Card {
                 display.append("Ace");
                 break;    
             default:
-                display.append(value); //number from 2 to 10 does not need to modify
+                display.append(value); // does not need to modify
                 break;
         }//end rank switch
         
-        display.append(" of "); //setting the format of the output
+        display.append(" of "); // the format of the output
         
         switch(suit){
             case 0:
@@ -67,11 +67,11 @@ public class Card {
             case 3:
                 display.append("Diamonds");
                 break;
-            default: //anything else, do nothing
+            default: 
                 break;
         }//end suit switch
         
-        //return the result of an entire cmombined string
+        // result of an entire cmombined string
         return display.toString();
     }//end toString
     
