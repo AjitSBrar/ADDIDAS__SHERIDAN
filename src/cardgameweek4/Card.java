b550691d756992c0c3e38f1e033b51ee8779a7ec
AjitSBrar <braraj@sheridancollege.ca>
2019-06-12 02:35:30 -0400

    Addidas Sheridan Deleveriable 1

---
+++ src/cardgameweek4/Card.java
@@ -0,0 +1,78 @@
+package cardgameweek4;
+/**
+ * Starter code for the Card class.
+ * 
+ * @author Addidas Sheridan 2019
+ */
+public class Card {
+    
+          private int value; //initialize the rank (2,3,4...King, Ace)
+    private int suit; //initialize the suit (spades, hearts...)
+    
+    //constructor
+    public Card(int s, int gVal){
+        value = gVal;
+        suit = s;
+    }//end construcor
+    
+    //getter method
+    public int getValue(){
+        return value; 
+    }//end getCard
+    
+    //setter method
+    public void setCard(int gVal){
+        value = gVal;
+    }//end setCard
+    
+    @Override
+    public String toString(){
+    //combine rank and suit together into a single string(ex: Ace of Diamonds)
+
+        //suing StringBuilder for modifiability later on
+        StringBuilder display = new StringBuilder();
+        
+        //personal choice to use switch
+        switch(value){
+            //since rank is int type, now match int 11 to String jack...14 to Ace
+            case 11:
+                display.append("Jack");
+                break;
+            case 12:
+                display.append("Queen");
+                break;
+            case 13:
+                display.append("King");
+                break;
+            case 14:
+                display.append("Ace");
+                break;    
+            default:
+                display.append(value); //number from 2 to 10 does not need to modify
+                break;
+        }//end rank switch
+        
+        display.append(" of "); //setting the format of the output
+        
+        switch(suit){
+            case 0:
+                display.append("Spades");
+                break;
+            case 1:
+                display.append("Hearts");
+                break;
+            case 2:
+                display.append("Clubs");
+                break;
+            case 3:
+                display.append("Diamonds");
+                break;
+            default: //anything else, do nothing
+                break;
+        }//end suit switch
+        
+        //return the result of an entire cmombined string
+        return display.toString();
+    }//end toString
+    
+}//end Card Class
---
+++ src/cardgameweek4/GamePlayer.java
@@ -0,0 +1,110 @@
+package cardgameweek4;
+/**
+ * Starter code for the Card class.
+ * 
+ * @author Addidas Sheridan 2019
+ */
+
+import java.util.ArrayList;     //import ArrayList  
+import java.util.Random;        //import Random
+import java.util.List;          //import List
+import java.util.Collections;   //import Collections
+import java.util.LinkedList;    //import LinkList
+
+public class GamePlayer {
+    public static void main(String[] args) {
+        
+        List<Card> deck = new ArrayList<Card>(); //create an ArrayList "cardDeck"
+        
+        for(int x=0; x<4; x++){          //0-3 for suit (4 suits)
+            for(int y=2; y<15; y++){     //2-14 for rank (13 ranks)
+                deck.add(new Card(x,y)); //create new card and add into the deck
+            } //end value for
+        }//end suit for
+        
+        Collections.shuffle(deck, new Random()); //shuffle the deck 
+        
+        //creating 2 decks, each for player1/player2
+        LinkedList<Card> deckCPU = new LinkedList<Card>();
+        LinkedList<Card> deckP1 = new LinkedList<Card>();
+        
+        deckCPU.addAll(deck.subList(0, 25));              //26 cards        
+        deckP1.addAll(deck.subList(26, deck.size()));//26 cards
+        
+        while(true){
+            Card p1Card = deckCPU.pop();  //each player place one card face up
+            Card p2Card = deckP1.pop();
+            
+            //display the face up card
+            System.out.println("Player 1 plays card is " + p1Card.toString());
+            System.out.println("Player 2 plays card is " + p2Card.toString());
+            
+            //rank comparation between two cards
+            if(p1Card.getValue() > p2Card.getValue()){//if player 1 win 
+                deckCPU.addLast(p1Card);  //higher value wins both cards and 
+                deckCPU.addLast(p2Card);  //places them at the bottom of his deck.
+                System.out.println("PLayer 1 wins the round");
+            }//end if
+ 
+            else if(p1Card.getValue() < p2Card.getValue()){//if player 2 win 
+                deckP1.addLast(p1Card);   
+                deckP1.addLast(p2Card);  
+                System.out.println("PLayer 2 wins the round");
+            }//end else if
+            
+            else { //war happens when both cards' rank matched
+                System.out.println("War"); 
+                
+                //creating war cards
+                List<Card> war1 = new ArrayList<Card>(); 
+                List<Card> war2 = new ArrayList<Card>();
+                
+                //checking do players have enough (4)cards 
+                for(int x=0; x<3; x++){ 
+                    //either one player runs out of card is game over
+                    if(deckCPU.size() == 0 || deckP1.size() == 0 ){                      
+                        break;
+                    }//end if
+                    
+                    System.out.println("War card for player1 is xx\nWar card for player2 is xx");
+
+                    war1.add(deckCPU.pop());  //place additional card for war
+                    war2.add(deckP1.pop());                  
+                }//end for
+                
+                //only compare result when both players have enough cards for war
+                if(war1.size() == 3 && war2.size() == 3 ){
+                    //display the war cards from each player
+                    System.out.println("War card for player1 is " + war1.get(0).toString());
+                    System.out.println("War card for player2 is " + war2.get(0).toString());
+                    
+                    //if player 1 wins the war round
+                    if(war1.get(2).getValue() > war2.get(2).getValue()){
+                        deckCPU.addAll(war1); //player1 get all 10 cards
+                        deckCPU.addAll(war2);
+                        System.out.println("Player 1 wins the war round");
+                    }//end if
+                    //otherwise player 2 wins the war round
+                    else{
+                        deckP1.addAll(war1); //player2 get all 10 cards
+                        deckP1.addAll(war2);
+                        System.out.println("Player 2 wins the war round");
+                    }//end else                      
+                }//end if
+                
+            }//end war round else
+            
+            //game over either one player runs out of card(deck size is 0)
+            if(deckCPU.size() == 0 ){
+                System.out.println("game over\nPlayer 1 wins the game");
+                break;
+            }
+            else if(deckP1.size() == 0){
+                System.out.println("game over\nPlayer 2 wins the game");
+                break;
+            }
+           
+        }//end while  
+
+    }//end main       
+}//end class
